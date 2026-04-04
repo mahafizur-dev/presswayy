01b58ead59b1d30cbe1b2a61d3e53031595aa88f
Mahafizur Rahman <LnG2509P0074@clx.dc>
2026-04-04 18:36:14 +0600

    update gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,17 +22,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NPBBD33T');
-          `}
-        </Script>
-      </head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NPBBD33T');
+        `}
+      </Script>
 
       <body
         className={`${inter.className} bg-white text-gray-900`}
@@ -47,12 +45,8 @@ export default function RootLayout({
           ></iframe>
         </noscript>
 
-        {/* Navbar added here so it appears on all pages */}
         <Navbar />
-
-        {/* Main page content will be rendered here */}
         <main className="min-h-screen">{children}</main>
-
         <Footer />
       </body>
     </html>
